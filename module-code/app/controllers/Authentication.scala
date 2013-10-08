804813424df24aba9028d74010bc9ecdd7919eb8
Nick Childers <nick@jobhive.com>
2013-10-08 16:52:46 -0700

    Adding support for Play 2.2

--- module-code/app/controllers/Authentication.scala
+++ module-code/app/controllers/Authentication.scala
@@ -3,12 +3,14 @@ package reactivesecurity.controllers
 import play.api.mvc._
 
 import scala.concurrent.{Future, ExecutionContext}
-import reactivesecurity.core.Authentication.AsyncAuthentication
+import reactivesecurity.core.Authentication.{AuthenticationFailureHandler, AsyncInputValidator, AsyncAuthentication}
 import reactivesecurity.core.std.AuthenticationFailure
 import reactivesecurity.core.User.UsingID
 import play.api.libs.iteratee.{Iteratee, Enumerator}
 import play.api.mvc.WebSocket.FrameFormatter
+import play.api.mvc.BodyParsers.parse
 
+/*
 trait Authentication[USER <: UsingID] extends Controller with AsyncAuthentication[RequestHeader,Result,USER,AuthenticationFailure] {
 
   def FutureAuthenticated[A](p: BodyParser[A])(f: Request[A] => USER => Future[Result])(implicit ec: ExecutionContext): Action[A] =
@@ -45,6 +47,49 @@ trait Authentication[USER <: UsingID] extends Controller with AsyncAuthenticatio
         succ = { user => Some(user) }
       )
     }
+  }
+}*/
+
+
+
+
+trait Authentication2[USER <: UsingID] extends AsyncAuthentication[RequestHeader,Future[SimpleResult],USER,AuthenticationFailure] {
+  import ExecutionContext.Implicits.global
+  class AuthenticatedRequest[A](val user: USER, request: Request[A]) extends WrappedRequest[A](request)
+
+  case class Authenticated[A](action: Action[A]) extends Action[A] {
+    def apply(request: Request[A]): Future[SimpleResult] = {
+      inputValidator.validateInput(request).flatMap { validation =>
+        validation.fold(
+          fail = { f => authFailureHandler.onAuthenticationFailure(request,f) },
+          succ = { user => action(new AuthenticatedRequest[A](user,request)) }
+        )
+      }
+    }
+    lazy val parser = action.parser
+  }
+
+  def AuthenticatedWS[A](f: RequestHeader => USER => Future[(Iteratee[A, _], Enumerator[A])])(implicit frameFormatter: FrameFormatter[A], ec: ExecutionContext): WebSocket[A] = WebSocket.async[A] {
+    request => inputValidator.validateInput(request).flatMap { result =>
+      val foo: Iteratee[A,Nothing] = play.api.libs.iteratee.Error("Not Authorized",play.api.libs.iteratee.Input.Empty)
+      result.fold(
+        fail => concurrent.future { (foo,Enumerator.eof) },
+        user => f(request)(user)
+      )
+    }
+  }
+
+  object Authenticated extends ActionBuilder[AuthenticatedRequest] {
+    override def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[SimpleResult]) = {
+      inputValidator.validateInput(request).flatMap { validation =>
+        validation.fold(
+          fail = { f => authFailureHandler.onAuthenticationFailure(request,f) },
+          succ = { user => block(new AuthenticatedRequest[A](user,request)) }
+        )
+      }
+    }
+
+    override def composeAction[A](action: Action[A]) = new Authenticated(action)
   }
 }
 
--- module-code/project/Build.scala
+++ module-code/project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.scalaz" %% "scalaz-core" % "7.0.1",
+    cache,
+    "org.scalaz" %% "scalaz-core" % "7.0.3",
     "org.mindrot" % "jbcrypt" % "0.3m"
   )
 
